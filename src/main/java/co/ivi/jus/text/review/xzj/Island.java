c88c4accb127ee593b4a494ea2b88be9b558221a
xzj <xzj007@gmail.com>
2021-11-18 18:05:49 +0800

    Added homework of lesson 2.

---
+++ src/main/java/co/ivi/jus/text/review/xzj/Island.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2021, Zhijie Xu. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ */
+
+package co.ivi.jus.text.review.xzj;
+
+public class Island {
+    public static void main(String[] args) {
+        System.out.println("Space leading:");
+        System.out.println("""
+                           No man is an island,
+                             Entire of itself,
+                    Every man is a piece of the continent,
+                            A part of the main.
+                    """);
+
+        System.out.println("Tab leading: ");
+        System.out.println("""
+                           No man is an island,
+                             Entire of itself,
+                    Every man is a piece of the continent,
+                            A part of the main.
+                    """);
+    }
+}
