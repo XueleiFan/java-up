cf7c2e6bb3ee4d41ee07d1393427355e9d1c9ab2
songor <chensongyu1028@gmail.com>
2021-11-21 03:06:44 +0800

    修改 Copyright

--- src/main/java/co/ivi/jus/text/review/songor/Island.java
+++ src/main/java/co/ivi/jus/text/review/songor/Island.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021, Xuelei Fan. All rights reserved.
+ * Copyright (c) 2021, songor. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  */
 
