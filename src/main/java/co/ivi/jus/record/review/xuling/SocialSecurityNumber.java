4c1a7d5407e6c95657bfdec7429a2cc44ea6c228
Jack Xu <xulingcom@126.com>
2021-11-24 15:16:47 +0800

    feat(record): rewirite SocialSecurityNumber's equals,hascode and toString method

---
+++ src/main/java/co/ivi/jus/record/review/xuling/SocialSecurityNumber.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2021, Ling Xu. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ */
+
+package co.ivi.jus.record.review.xuling;
+
+import java.util.Arrays;
+
+/**
+ * record class work
+ *
+ * @author jack
+ * @since 2021/11/24
+ **/
+public record SocialSecurityNumber(byte[] ssn) {
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+
+        if (o == null) {
+            return false;
+        }
+
+        if (o instanceof SocialSecurityNumber that) {
+            return Arrays.equals(ssn, that.ssn());
+        }
+
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(ssn);
+    }
+
+    @Override
+    public String toString() {
+        return "SocialSecurityNumber{" +
+                "ssn=" + Arrays.toString(ssn) +
+                '}';
+    }
+}
