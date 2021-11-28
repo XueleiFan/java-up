6cb904f26b2b78eac1e760b43dae00175e030368
songor <chensongyu1028@gmail.com>
2021-11-28 17:16:04 +0800

    fix(03): code review

--- src/main/java/co/ivi/jus/record/review/songor/SocialSecurityNumber.java
+++ src/main/java/co/ivi/jus/record/review/songor/SocialSecurityNumber.java
@@ -6,6 +6,7 @@
 package co.ivi.jus.record.review.songor;
 
 import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
 import java.util.Arrays;
 
 record SocialSecurityNumber(byte[] ssn) {
@@ -15,11 +16,10 @@ record SocialSecurityNumber(byte[] ssn) {
         }
     }
 
-    @Override
-    public byte[] ssn() {
+    public String show() {
         String str = new String(ssn, StandardCharsets.UTF_8);
         String regex = String.format("(?<=\\w{%d})\\w(?=\\w{%d})", 6, 4);
-        return str.replaceAll(regex, "*").getBytes(StandardCharsets.UTF_8);
+        return str.replaceAll(regex, "*");
     }
 
     @Override
@@ -27,11 +27,10 @@ record SocialSecurityNumber(byte[] ssn) {
         if (this == o) {
             return true;
         }
-        if (o == null || getClass() != o.getClass()) {
+        if (!(o instanceof SocialSecurityNumber other)) {
             return false;
         }
-        SocialSecurityNumber other = (SocialSecurityNumber) o;
-        return Arrays.equals(ssn, other.ssn);
+        return MessageDigest.isEqual(ssn, other.ssn);
     }
 
     @Override
